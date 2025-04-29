1d164e015f08243d6c837b214dc7ea451880d94f
LeonardPM <leonardpaccham@gmail.com>
2025-04-28 23:14:09 -0500

    Tarea: Problema del 6

---
+++ Solucion_Codigo/src/problema6_EstudianteEjecutor.java
@@ -0,0 +1,6 @@
+public class problema6_EstudianteEjecutor {
+    public static void main(String[] args) {
+        problema6_Estudiante p = new problema6_Estudiante("Ana", "Lopez", "0102030405", 1200.0);
+        System.out.println(p);
+    }
+}
